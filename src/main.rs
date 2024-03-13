05b0cf99d3aec5ff4ad4de9ca3516adbae9a15f2
Aditya Gupta <116362593+adisuyash@users.noreply.github.com>
2024-03-13 07:39:48 +0530

    Add rand crate & started!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+// Importing the rand crate/library
+use rand::Rng;
+
+// Importing the io module from the standard library
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    // Generating a random number between 1 and 10
+    let secret_number = rand::thread_rng().gen_range(1..11);
+
+    // Printing the secret number
+    let mut attempts = 0;
 }
