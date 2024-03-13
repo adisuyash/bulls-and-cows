69510308a4e92660a90d0232f98d247acd7e7ba6
Aditya Gupta <116362593+adisuyash@users.noreply.github.com>
2024-03-13 07:45:41 +0530

    Refactor game logic and improve user experience

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,59 @@
-// Importing the rand crate/library
+use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
-// Importing the io module from the standard library
-use std::io;
-
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to Bull and Cows");
 
-    // Generating a random number between 1 and 10
     let secret_number = rand::thread_rng().gen_range(1..11);
 
-    // Printing the secret number
     let mut attempts = 0;
+
+    loop {
+        println!("Please input a number:");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Ops! Something goes wrong");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input valid number");
+                continue;
+            }
+        };
+
+        attempts += 1;
+
+        if guess < 1 || guess > 10 {
+            println!("Please input a number between 1 and 10");
+            continue;
+        }
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => {
+                println!("too small!");
+                if attempts > 5 {
+                    println!("tips: you have tried {} times", attempts);
+                }
+            }
+            Ordering::Greater => {
+                println!("too big!");
+                if attempts > 5 {
+                    println!("tips: you have tried {} times", attempts);
+                }
+            }
+            Ordering::Equal => {
+                println!("Congratulation you're right!");
+                println!("tips: you have tried {} times", attempts);
+                break;
+            }
+        }
+
+        if attempts == 10 {
+            println!("You have tried 10 times, game over");
+            break;
+        }
+    }
 }
